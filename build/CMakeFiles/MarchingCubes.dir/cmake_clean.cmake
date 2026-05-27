d50384812fa92e8ca885ca4a87718dc0d739e496
Stalhein <erich.stalhein@proton.me>
2026-05-27 20:52:24 +0000

    Initial commit

---
+++ build/CMakeFiles/MarchingCubes.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MarchingCubes.dir/link.d"
+  "CMakeFiles/MarchingCubes.dir/external/glad/src/glad.c.o"
+  "CMakeFiles/MarchingCubes.dir/external/glad/src/glad.c.o.d"
+  "CMakeFiles/MarchingCubes.dir/src/main.cpp.o"
+  "CMakeFiles/MarchingCubes.dir/src/main.cpp.o.d"
+  "MarchingCubes"
+  "MarchingCubes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/MarchingCubes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
