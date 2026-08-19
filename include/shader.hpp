828cc2a6c6aa11024478e7c379afb3d8e7c362e2
Stalhein <erich.stalhein@proton.me>
2026-08-19 20:14:44 +0100

    Commit #13 to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop. Currently refactoring codebase from using 2 space tabs to using 4 space tabs.

--- include/shader.hpp
+++ include/shader.hpp
@@ -79,63 +79,63 @@ public:
         checkCompileErrors(ID, "PROGRAMME");
     
         glDeleteShader(vertex);
-    glDeleteShader(fragment);
-  }
+        glDeleteShader(fragment);
+    }
 
-  void use() const { glUseProgram(ID); }
+    void use() const { glUseProgram(ID); }
 
-  void setBool(const std::string &name, bool value) const {
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-  }
+    void setBool(const std::string &name, bool value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
 
-  void setInt(const std::string &name, int value) const {
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-  }
+    void setInt(const std::string &name, int value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
 
-  void setFloat(const std::string &name, float value) const {
-    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-  }
+    void setFloat(const std::string &name, float value) const {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
 
-  void setVec3(const std::string &name, const glm::vec3 &value) const {
-    glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-  }
+    void setVec3(const std::string &name, const glm::vec3 &value) const {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
 
-  void setVec3(const std::string &name, float x, float y, float z) const {
-    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
-  }
+    void setVec3(const std::string &name, float x, float y, float z) const {
+        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+    }
 
-  void setMat4(const std::string &name, const glm::mat4 &value) const {
-    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &value[0][0]);
-  }
+    void setMat4(const std::string &name, const glm::mat4 &value) const {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &value[0][0]);
+    }
 
 private:
-  static std::string readFile(const char* path) {
-    std::ifstream file(path);
+    static std::string readFile(const char* path) {
+        std::ifstream file(path);
 
-    if (!file.is_open()) {
-      throw std::runtime_error(std::string("Failed to open ", path));
+        if (!file.is_open()) {
+            throw std::runtime_error(std::string("Failed to open ", path));
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        return buffer.str();
     }
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
-  }
-
-  void checkCompileErrors(GLuint shader, std::string type) {
-    GLint success;
-    GLchar infoLog[512];
-    if (type != "PROGRAMME") {
-      glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-      if (!success) {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << std::endl;
-      }
-    } else {
-      glGetProgramiv(shader, GL_LINK_STATUS, &success);
-      if (!success) {
-        glGetProgramInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR::PROGRAM_LINKING_ERROR\n" << infoLog << std::endl;
-      }
+    void checkCompileErrors(GLuint shader, std::string type) {
+        GLint success;
+        GLchar infoLog[512];
+        if (type != "PROGRAMME") {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(shader, 512, NULL, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << std::endl;
+            }
+        } else {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shader, 512, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR\n" << infoLog << std::endl;
+            }
+        }
     }
-  }
 };
