c7f0f44df77bf2bf03782ece86da36d099e7a5b5
Stalhein <erich.stalhein@proton.me>
2026-08-19 20:08:40 +0100

    Commit #11 to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop. Currently refactoring codebase from using 2 space tabs to using 4 space tabs.

--- include/camera.hpp
+++ include/camera.hpp
@@ -10,67 +10,67 @@ constexpr float SENSITIVITY = 0.1f;
 
 class Camera {
 public:
-  glm::vec3 position, front, frontXZ, right, up, worldUp;
-  float yaw, pitch;
+    glm::vec3 position, front, frontXZ, right, up, worldUp;
+    float yaw, pitch;
 
-  Camera(glm::vec3 startPosition, float startYaw, float startPitch)
-    : position(startPosition), yaw(startYaw), pitch(startPitch),
-    front(glm::vec3(0.f)), frontXZ(glm::vec3(0.f)),
-    up(glm::vec3(0.f, 1.f, 0.f)), worldUp(up) {
-    updateCameraVectors();
-  }
-
-  glm::mat4 getViewMatrix() {
-    return glm::lookAt(position, position + front, up);
-  }
-
-  void processKeyboard(CameraDirections dir, float deltaTime) {
-    float v = SPEED * deltaTime;
-
-    switch(dir) {
-      case FORWARD:
-        position += frontXZ * v;
-        break;
-      case BACKWARD:
-        position -= frontXZ * v;
-        break;
-      case UP:
-        position += worldUp * v;
-        break;
-      case DOWN:
-        position -= worldUp * v;
-        break;
-      case RIGHT:
-        position += right * v;
-        break;
-      case LEFT:
-        position -= right * v;
-        break;
+    Camera(glm::vec3 startPosition, float startYaw, float startPitch)
+        : position(startPosition), yaw(startYaw), pitch(startPitch),
+        front(glm::vec3(0.f)), frontXZ(glm::vec3(0.f)),
+        up(glm::vec3(0.f, 1.f, 0.f)), worldUp(up) {
+        updateCameraVectors();
     }
-  }
 
-  void processMouse(float xoffset, float yoffset) {
-    xoffset *= SENSITIVITY;
-    yoffset *= SENSITIVITY;
+    glm::mat4 getViewMatrix() {
+        return glm::lookAt(position, position + front, up);
+    }
 
-    yaw += xoffset;
-    pitch += yoffset;
+    void processKeyboard(CameraDirections dir, float deltaTime) {
+        float v = SPEED * deltaTime;
 
-    if (pitch > 89.f) pitch = 89.f;
-    else if (pitch < -89.f) pitch = -89.f;
+        switch(dir) {
+            case FORWARD:
+                position += frontXZ * v;
+                break;
+            case BACKWARD:
+                position -= frontXZ * v;
+                break;
+            case UP:
+                position += worldUp * v;
+                break;
+            case DOWN:
+                position -= worldUp * v;
+                break;
+            case RIGHT:
+                position += right * v;
+                break;
+            case LEFT:
+                position -= right * v;
+                break;
+            }
+        }
 
-    updateCameraVectors();
-  }
+    void processMouse(float xoffset, float yoffset) {
+        xoffset *= SENSITIVITY;
+        yoffset *= SENSITIVITY;
+
+        yaw += xoffset;
+        pitch += yoffset;
+
+        if (pitch > 89.f) pitch = 89.f;
+        else if (pitch < -89.f) pitch = -89.f;
+
+        updateCameraVectors();
+    }
 private:
-  void updateCameraVectors() {
-    glm::vec3 f;
-    f.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    f.y = 0.f;
-    f.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-    frontXZ = glm::normalize(f);
-    f.y = sin(glm::radians(pitch));
-    front = glm::normalize(f);
-    right = glm::normalize(glm::cross(front, worldUp));
-    up = glm::normalize(glm::cross(right, front));
-  }
+    void updateCameraVectors() {
+        glm::vec3 f;
+        f.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        f.y = 0.f;
+        f.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        frontXZ = glm::normalize(f);
+        f.y = sin(glm::radians(pitch));
+        front = glm::normalize(f);
+        right = glm::normalize(glm::cross(front, worldUp));
+        up = glm::normalize(glm::cross(right, front));
+    }
 };
--- include/chunk.hpp
+++ include/chunk.hpp
@@ -15,36 +15,39 @@
 
 class Chunk {
 public:
-  glm::vec3 position;
-  glm::mat4 model;
+    glm::vec3 position;
+    glm::mat4 model;
 
-  Chunk(glm::vec3 chunkPosition);
+    Chunk(glm::vec3 chunkPosition);
 
-  void generate();
+    void generate();
 
-  void render();
+    void render();
 
 private:
-  GLuint vbo, vao;
-  float paddedDensities[(SIZE+3)*(SIZE+3)*(SIZE+3)];
-  glm::vec3 gradients[(SIZE+1)*(SIZE+1)*(SIZE+1)];
-  std::vector<float> vertices;
-  FastNoise::SmartNode<FastNoise::Simplex> main;
-  FastNoise::SmartNode<FastNoise::FractalRidged> mainFractal;
-  FastNoise::SmartNode<FastNoise::Simplex> detail;
-  FastNoise::SmartNode<FastNoise::FractalFBm> detailFractal;
-  float heightMap[(SIZE+3)*(SIZE+3)];
-  float detailDensities[(SIZE+3)*(SIZE+3)*(SIZE+3)];
+    GLuint vbo, vao;
+    float paddedDensities[(SIZE+3)*(SIZE+3)*(SIZE+3)];
+    glm::vec3 gradients[(SIZE+1)*(SIZE+1)*(SIZE+1)];
+    std::vector<float> vertices;
+    FastNoise::SmartNode<FastNoise::Simplex> main;
+    FastNoise::SmartNode<FastNoise::FractalRidged> mainFractal;
+    FastNoise::SmartNode<FastNoise::Simplex> detail;
+    FastNoise::SmartNode<FastNoise::FractalFBm> detailFractal;
+    float heightMap[(SIZE+3)*(SIZE+3)];
+    float detailDensities[(SIZE+3)*(SIZE+3)*(SIZE+3)];
 
-  float getDensity(float x, float y, float z);
-  glm::vec3 getNormal(int x, int y, int z);
-  int idxGradients(int x, int y, int z) {
-    return z * (SIZE+1) * (SIZE+1) + y * (SIZE+1) + x;
-  }
-  int idx(int x, int y, int z) {
-    int nx = x+1;
-    int ny = y+1;
-    int nz = z+1;
-    return nz * (SIZE+3) * (SIZE+3) + ny * (SIZE+3) + nx;
-  }
+    float getDensity(float x, float y, float z);
+
+    glm::vec3 getNormal(int x, int y, int z);
+
+    int idxGradients(int x, int y, int z) {
+        return z * (SIZE+1) * (SIZE+1) + y * (SIZE+1) + x;
+    }
+
+    int idx(int x, int y, int z) {
+        int nx = x+1;
+        int ny = y+1;
+        int nz = z+1;
+        return nz * (SIZE+3) * (SIZE+3) + ny * (SIZE+3) + nx;
+    }
 };
