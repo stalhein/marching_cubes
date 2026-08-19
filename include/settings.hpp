ae8ca937d4dfe4eb800e485bf33a6d27206afa46
Stalhein <erich.stalhein@proton.me>
2026-08-19 20:17:47 +0100

    Commit #14 to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop. Currently refactoring codebase from using 2 space tabs to using 4 space tabs.

--- include/settings.hpp
+++ include/settings.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 constexpr int SIZE = 32;
 constexpr int CHUNK_HEIGHT = 6;
 constexpr int WORLD_HEIGHT = SIZE * CHUNK_HEIGHT;
@@ -8,3 +10,8 @@ constexpr float ISO = 0.f;
 
 constexpr int SCR_WIDTH = 800;
 constexpr int SCR_HEIGHT = 600;
+
+int multiplication_of_prime_factors(int i) {
+    // Get prime factors
+    std::vector<int> factors;
+}
