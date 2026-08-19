fee640055d0157d593eae0e8c9796b6d7fc108b1
Stalhein <erich.stalhein@proton.me>
2026-08-19 20:55:14 +0100

    Commit #25 to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop. Currently verbose mode for debugging.

--- src/logger.cpp
+++ src/logger.cpp
@@ -3,12 +3,19 @@
 #include <iostream>
 #include <unistd.h>
 
+constexpr const char* RESET = "\033[0m";
+constexpr const char* RED = "\033[31m";
+constexpr const char* YELLOW = "\033[33m";
+constexpr const char* GREEN = "\033[32m";
+constexpr const char* CYAN = "\033[36m";
+constexpr const char* GREY = "\033[90m";
+
 bool Logger::verbose = false;
 
 bool Logger::useColour = [] {
     if (std::getenv("NO_COLOR") != nullptr) return false;
 
-    return isatty(STDOUT_FILENO);
+    return (bool)(isatty(STDOUT_FILENO));
 }();
 
 void Logger::setVerbose(bool enabled) {
