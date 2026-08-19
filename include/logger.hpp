2df2c4403ada192f4bc8f85c238f41995794d96d
Stalhein <erich.stalhein@proton.me>
2026-08-19 20:43:03 +0100

    Commit #21 to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop. Currently verbose mode for debugging.

---
+++ include/logger.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class Logger {
+public:
+    static void info(const std::string &message);
+    static void debug(const std::string &message);
+    static void warning(const std::string &message);
+    static void error(const std::string &message);
+
+    static void setVerbose(bool enabled);
+
+private:
+    static bool verbose;
+};
