4c70b1cddb4ff69484b95d7322759f3d2f49079f
Stalhein <erich.stalhein@proton.me>
2026-08-19 19:36:53 +0100

    Commit #1 to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop.

---
+++ git_pusher.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+
+int str_to_int(char *str, int len) {
+    int result = 0;
+    for (int i = 0; i < len; ++i) {
+        int base = len - i - 1;
+        result += (*str++ - '0') * pow(10, base);
+    }
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) return -1;
+    int pause = str_to_int(argv[1], strlen(argv[1]));
+
+    int commit = 1;
+
+    while (1) {
+        char command[512];
+        snprintf(command, sizeof(command),
+                "git add . && git commit -m \"Commit #%d to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop.\" && git push", commit);
+
+        int r = system(command);
+        if (r != 0) {
+            printf("Git commit failed (%d)\n", r);
+        }
+
+        commit++;
+        sleep(pause);
+    }
+
+    return 0;
+}
