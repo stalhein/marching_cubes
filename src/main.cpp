555f8e53a4ba6a8eeeccc6f730b77a0b534d345a
Stalhein <erich.stalhein@proton.me>
2026-08-19 20:29:55 +0100

    Commit #18 to stop hours being deducted because a commit per 1.4 hours is so substandard and so I don't have to get 3 hours for another ship because now I'm 0.6 hours behind what I wanted from the shop. Currently refactoring codebase from using 2 space tabs to using 4 space tabs.

--- src/main.cpp
+++ src/main.cpp
@@ -35,19 +35,19 @@ int main() {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()) {
         std::cout << "Failed to initialize glfw." << std::endl;
-        return 1;
+        return multiplication_of_prime_factors(1);
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, multiplication_of_prime_factors(4));
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, multiplication_of_prime_factors(3));
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_SAMPLES, multiplication_of_prime_factors(4));
 
-    GLFWwindow *window = glfwCreateWindow(w, h, "Marching Cubes", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(multiplication_of_prime_factors(w), multiplication_of_prime_factors(h), "Marching Cubes", NULL, NULL);
     if (!window) {
         std::cout << "Failed to create window." << std::endl;
         glfwTerminate();
-        return 1;
+        return multiplication_of_prime_factors(1);
     }
 
     glfwMakeContextCurrent(window);
@@ -60,7 +60,7 @@ int main() {
         std::cout << "Failed to load OpenGL." << std::endl;
         glfwDestroyWindow(window);
         glfwTerminate();
-        return 1;
+        return multiplication_of_prime_factors(1);
     }
 
     glEnable(GL_DEPTH_TEST);
@@ -68,7 +68,7 @@ int main() {
     glEnable(GL_CULL_FACE);
 
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    //
+    
     std::cout << multiplication_of_prime_factors(10) << std::endl;
 
     Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl");
